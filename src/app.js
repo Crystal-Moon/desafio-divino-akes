519065d1f12f984e82a6ae5ec9857b7a9b515a9b
CrystalMoon <crystal.marino345@gmail.com>
2020-12-19 21:48:00 -0300

    funcional window y onTops

--- src/app.js
+++ src/app.js
@@ -1,10 +1,4 @@
 
-
-//import {AppLib} from './app-core/lib';
-//import './mycss.css';
-//import './mycss2.css';
-
-//let appLib = new AppLib();
 let runOnTop = function() {
   Neutralino.os.runCommand('onTop.exe', 
     function (data) {}, 
@@ -12,22 +6,10 @@ let runOnTop = function() {
       alert("Surgió un error con la funcion 'siempre visible', utilice la forma alternativa");
     })
 }
-/*
-let myapp = {
-    myfunction : function () { 
-        document.getElementById('info').innerHTML = NL_NAME + 
-        " is running on port " +
-        NL_PORT + " inside " + NL_OS + "<br/><br/>" 
-        + "<span>v" + NL_VERSION + "</span>"; 
-    }
-};
-*/
 
 Neutralino.init({
     load: function() {
         window.setMyTop= runOnTop;
-        //myapp.myfunction();
-        //appLib.showSettings();
     },
     pingSuccessCallback : function() {
 
